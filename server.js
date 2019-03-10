6f6f9cb953d463589f924629d136079ab9e1f912
enliberte <zugife21@gmail.com>
2019-03-10 17:53:06 +0300

    'listen'

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const {Client} = require('pg');
 app = express();
 port = process.env.PORT || 8000;
 
+app.listen(port, function () {
+    console.log('App is running on port ' + port);
+});
+
 //отдача статики
 app.use('/js', express.static(__dirname + '/js'));
 app.use('/css', express.static(__dirname + '/css'));
